353956c5036c8e836f996618d5f1a0bfda5cfbae
Lukas Silva <lukasg.dasilva2014@gmail.com>
2023-10-20 10:36:20 -0300

    new things

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="text 2xl pt-12">
       <h1 className="text-3xl">Building...</h1>
+      <h1>navegue através do menu</h1>
     </div>
   );
 }
